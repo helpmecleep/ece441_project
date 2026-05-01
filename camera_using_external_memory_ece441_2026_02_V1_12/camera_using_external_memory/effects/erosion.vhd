09426b3008d2f40673cfba7df7253509e3773bd4
Saleh <74556095+helpmecleep@users.noreply.github.com>
2026-05-01 15:46:16 -0700

    Update erosion.vhd

--- camera_using_external_memory_ece441_2026_02_V1_12/camera_using_external_memory/effects/erosion.vhd
+++ camera_using_external_memory_ece441_2026_02_V1_12/camera_using_external_memory/effects/erosion.vhd
@@ -82,11 +82,11 @@ erosion_window_3_by_3( 2, 2 ) <= erosion_line_buffer(2 + to_integer( SCREEN_X_SI
 -- erosion calculation: find min of the 3x3 window
 --
 
-min_row0 <= minimum(minimum(erosion_window_3_by_3(0,0), erosion_window_3_by_3(0,1)), erosion_window_3_by_3(0,2));
-min_row1 <= minimum(minimum(erosion_window_3_by_3(1,0), erosion_window_3_by_3(1,1)), erosion_window_3_by_3(1,2));
-min_row2 <= minimum(minimum(erosion_window_3_by_3(2,0), erosion_window_3_by_3(2,1)), erosion_window_3_by_3(2,2));
+min_row0 <= std_logic_vector(minimum(minimum(unsigned(erosion_window_3_by_3(0,0)), unsigned(erosion_window_3_by_3(0,1))), unsigned(erosion_window_3_by_3(0,2))));
+min_row1 <= std_logic_vector(minimum(minimum(unsigned(erosion_window_3_by_3(1,0)), unsigned(erosion_window_3_by_3(1,1))), unsigned(erosion_window_3_by_3(1,2))));
+min_row2 <= std_logic_vector(minimum(minimum(unsigned(erosion_window_3_by_3(2,0)), unsigned(erosion_window_3_by_3(2,1))), unsigned(erosion_window_3_by_3(2,2))));
 
-erosion_result <= minimum(minimum(min_row0, min_row1), min_row2);
+erosion_result <= std_logic_vector(minimum(minimum(unsigned(min_row0), unsigned(min_row1)), unsigned(min_row2)));
 
 pixel_out <= erosion_result & erosion_result & erosion_result;
 
